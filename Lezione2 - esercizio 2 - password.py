989171270ea4ab378b4ad6a2eeec4d6c4d27d7ed
Annarosa Savarino <savarino@olomedia.it>
2022-09-16 14:12:15 +0200

    rimosso while

--- Lezione2 - esercizio 2 - password.py
+++ Lezione2 - esercizio 2 - password.py
@@ -2,10 +2,8 @@
 #controllo password
 password = 'Password_sicura'
 insert_password = raw_input('Per favore digita la password di accesso')
-while not insert_password:
-    insert_password = raw_input('Per favore digita la password di accesso')
+
+if password == insert_password:
+    print('Benvenuto!')
 else:
-    if password == insert_password:
-        print('Benvenuto!')
-    else:
-        print('Password non corretta')
+    print('Password non corretta')
