edf485806df804bd4f90e090530c215520cbee2f
Annarosa Savarino <savarino@olomedia.it>
2022-09-16 15:44:04 +0200

    Lezione 3 - funzioni built-in

---
+++ Lezine 3 - funzioni built-in.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# set() > Convertire una lista in un insieme
+list_1 = ['a', 'b', 'c', 'a', 'e', 'b']
+insieme = set(list_1)
+print(list_1)
+print(insieme)
+
+
+# len() > lunghezza di una lista, stringa ecc..
+print(len(list_1))
+print(len(insieme))
+
+# type() > indica di che tipo è un oggetto
+x = 1
+y = 'Stringa'
+print(type(x))
+print(type(y))
