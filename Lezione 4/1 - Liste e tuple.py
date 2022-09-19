47a2794b02d9a47a5d77b185fe344d398b4378e6
Annarosa Savarino <savarino@olomedia.it>
2022-09-19 15:29:47 +0200

    Lezione 4 esercizio 1

---
+++ Lezione 4/1 - Liste e tuple.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+# Liste e tuple
+tupla = ('a', 'b', 'c')
+lista = ['e', 'f', 'g', 'z']
+print(len(tupla))
+print(len(lista))
+list(tupla)
+tuple(lista)
+print(type(tupla))
+print(type(lista))
