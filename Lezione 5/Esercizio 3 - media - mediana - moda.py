2dec5a4140c968d03cdef9979a6e8cdfdad5dcf2
Annarosa Savarino <savarino@olomedia.it>
2022-09-29 14:27:05 +0200

    data di nascita, modifiche per rimozione ora

--- Lezione 5/Esercizio 3 - media - mediana - moda.py
+++ Lezione 5/Esercizio 3 - media - mediana - moda.py
@@ -1,7 +1,14 @@
-# calcolo media, mediana, moda tramite libreria numpy (naturalmente bisogna prima installarla)
-
-from numpy import mean
+# calcolo media, mediana, moda tramite
+import statistics
 
 voti = [29, 18, 18, 25, 28, 27, 19, 30, 20]
 
 # media aritmetica
+print(statistics.mean(voti))
+
+# mediana
+
+print(statistics.median(voti))
+
+# moda
+print(statistics.mode(voti))
