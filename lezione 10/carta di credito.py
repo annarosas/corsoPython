6adb1e62d2931d8aeef82f61528d217c9adc3756
Annarosa Savarino <savarino@olomedia.it>
2022-10-05 16:01:11 +0200

    Esercizio Carta credito (da completare)

--- lezione 10/carta di credito.py
+++ lezione 10/carta di credito.py
@@ -1,4 +1,5 @@
-# test template
+# esercizio carta di credito
+# definisco la classe
 class CartaCredito:
     def __init__(self, cf, nome, cognome, codice, mese, anno, cvv ):
         self.cf = cf
@@ -8,13 +9,15 @@ class CartaCredito:
         self.mese = mese
         self.anno = anno
         self.cvv = cvv
-
+# metodo per stampare le info
     def infoCarta(self):
         return f'CF Intestatario:{self.cf} Nome Intestatario:{self.nome} Cognome Intestatario:{self.cognome} Codice carta:{self.codice} Mese scadenza:{self.mese} Anno scadenza:{self.anno} CVV:{self.cvv}'
+# metodo credito carta
     def Credito(self, credito_disponibile=0):
         self.credito_disponibile=credito_disponibile
 
         return f'Credito disponibile:{self.credito_disponibile}'
+#metodo prelievo
     def prelievo(self, cifraPrelievo='0'):
         try:
             if(cifraPrelievo<=self.credito_disponibile):
@@ -23,7 +26,7 @@ class CartaCredito:
                 raise Exception(f'Credito non sufficiente')
         except Exception:
            return f'Errore.Credito non sufficiente.Il credito disponibile è di {self.credito_disponibile} EURO'
-
+#metodo versamento
     def versamento(self, cifraVersamento='0'):
         try:
             if (cifraVersamento > 0):
@@ -38,15 +41,16 @@ class CartaCredito:
 
 
 
-
+# definisco l'oggetto carta 1
 carta1 = CartaCredito('svrnrs87l63g273s', 'Anna', 'Savarino', '3020 345 1234 4335', '09', '2024', '000')
+# definisco il credito disponibile per l'oggetto carta 1
 carta1_credito = carta1.Credito(1000)
-
+# stampo le info di carta 1
 print(carta1.infoCarta())
 
-
+# chiedo all'utente che operazione vuole svolgere
 operazione = input('Che operazione vuoi effettuare? Digita "P" per prelievo, digita "V" per versamento, "S" per stampare il saldo').upper()
-
+# gestisco gli esiti delle operazioni
 if (operazione == 'P'):
     cifraPrelievo = int(input('Quanto vuoi prelevare?'))
     print(carta1.prelievo(cifraPrelievo))
