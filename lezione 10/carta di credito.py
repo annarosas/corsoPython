c76f010e39fdf35f65f174f095b4568e7d3510e7
Annarosa Savarino <savarino@olomedia.it>
2022-10-05 16:22:45 +0200

    Esercizio Carta credito (da completare)

--- lezione 10/carta di credito.py
+++ lezione 10/carta di credito.py
@@ -1,7 +1,12 @@
 # esercizio carta di credito
+import shutil
+import os
+import stat
+# creo un file sul quale registrare le operazioni
+
 # definisco la classe
 class CartaCredito:
-    def __init__(self, cf, nome, cognome, codice, mese, anno, cvv ):
+    def __init__(self, cf, nome, cognome, codice, mese, anno, cvv, credito ):
         self.cf = cf
         self.nome = nome
         self.cognome = cognome
@@ -9,29 +14,30 @@ class CartaCredito:
         self.mese = mese
         self.anno = anno
         self.cvv = cvv
+        self.credito = credito
 # metodo per stampare le info
     def infoCarta(self):
         return f'CF Intestatario:{self.cf} Nome Intestatario:{self.nome} Cognome Intestatario:{self.cognome} Codice carta:{self.codice} Mese scadenza:{self.mese} Anno scadenza:{self.anno} CVV:{self.cvv}'
 # metodo credito carta
-    def Credito(self, credito_disponibile=0):
-        self.credito_disponibile=credito_disponibile
+    def Credito(self):
+        return f'Credito disponibile: {self.credito}'
 
-        return f'Credito disponibile:{self.credito_disponibile}'
+        return f'Credito disponibile:{self.credito}'
 #metodo prelievo
     def prelievo(self, cifraPrelievo='0'):
         try:
-            if(cifraPrelievo<=self.credito_disponibile):
-                return f'Prelievo eseguito.Il tuo credito residuo è di: {self.credito_disponibile - cifraPrelievo} Euro'
+            if(cifraPrelievo<=self.credito):
+                return f'Prelievo eseguito.Il tuo credito residuo è di: {self.credito - cifraPrelievo} Euro'
             else:
                 raise Exception(f'Credito non sufficiente')
         except Exception:
-           return f'Errore.Credito non sufficiente.Il credito disponibile è di {self.credito_disponibile} EURO'
+           return f'Errore.Credito non sufficiente.Il credito disponibile è di {self.credito} EURO'
 #metodo versamento
     def versamento(self, cifraVersamento='0'):
         try:
             if (cifraVersamento > 0):
-                self.credito_disponibile = self.credito_disponibile + cifraVersamento
-                return f'Versamento effettutato.Credito totale disponibile: {self.credito_disponibile} EURO '
+                self.credito = self.credito + cifraVersamento
+                return f'Versamento effettutato.Credito totale disponibile: {self.credito} EURO '
             else:
                 raise Exception(f'Errore')
         except Exception:
@@ -42,14 +48,16 @@ class CartaCredito:
 
 
 # definisco l'oggetto carta 1
-carta1 = CartaCredito('svrnrs87l63g273s', 'Anna', 'Savarino', '3020 345 1234 4335', '09', '2024', '000')
-# definisco il credito disponibile per l'oggetto carta 1
-carta1_credito = carta1.Credito(1000)
+carta1 = CartaCredito('svrnrs87l63g273s', 'Anna', 'Savarino', '3020 345 1234 4335', '09', '2024', '000', 1000)
+
 # stampo le info di carta 1
 print(carta1.infoCarta())
 
 # chiedo all'utente che operazione vuole svolgere
 operazione = input('Che operazione vuoi effettuare? Digita "P" per prelievo, digita "V" per versamento, "S" per stampare il saldo').upper()
+if operazione:
+    with open('operazioni_bancomat.txt', 'a') as file:
+        file.write(f'operazioni carta {carta1.infoCarta()} + \n')
 # gestisco gli esiti delle operazioni
 if (operazione == 'P'):
     cifraPrelievo = int(input('Quanto vuoi prelevare?'))
@@ -60,6 +68,6 @@ elif (operazione == 'V'):
     print(carta1.versamento(cifraVersamento))
 
 elif (operazione == 'S'):
-    print(carta1_credito)
+    print(carta1.Credito())
 
 
