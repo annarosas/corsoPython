0a351aa5cfb4d01e785e78164a71a3866d55afbe
annarosas <113357153+annarosas@users.noreply.github.com>
2022-11-08 18:52:06 +0100

    Delete esercizio2.B.py

--- corsoPython Avanzato/lezione3/esercizio2.B.py
+++
@@ -1,9 +0,0 @@
-# 2) Scrivere un programma Python che:
-# A. Richiede all’utente di inserire un numero N compreso tra 5 e 15
-# B. Richiede all’utente di inserire N elementi e li memorizza in una lista L
-# C. Creare una copia L2 della lista L
-# D. Modificare il valore di N/3 elementi della lista L2 scelti in modo casuale
-# con valori casuali tra 0 e 100 (valutare se opportuno scrivere una
-# funzione)
-# E. Stampare (utilizzare la funzione sommaElem(list) dell’esercizio 1) il
-# valore della lista L e della lista L2
