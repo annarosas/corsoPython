79b0e81755d0ee8f0a7f79b76666d7ceb8c5e03f
Annarosa Savarino <savarino@olomedia.it>
2022-11-09 14:40:00 +0100

    esercizio 2

--- corsoPython Avanzato/lezione3/esercizio1.py
+++ corsoPython Avanzato/lezione3/esercizio1.py
@@ -1,9 +1,10 @@
 # Scrivere una funzione sommaElem(list) che riceve in input una list (list) e
 # restituisce la somma degli elementi della lista (list)
 def sommaElement(list):
-    print(sum(list))
+    return sum(list)
 
 
 lista =[1,3,4]
 
-sommaElement(lista)
+somma = sommaElement(lista)
+print(somma)
